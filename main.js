fdf3d11d218ef8ad4d25c26a88b4d63b9b606ca7
josh1510 <joshmoon.1510@gmail.com>
2021-05-28 18:19:02 +1000

    working on remove book button

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ let bookCount = 0;
 
 const libraryContainer = document.getElementById('library-container');
 
+libraryContainer.addEventListener('click', (event) => {
+    const isRemove = event.target.className;
+    if (isRemove === 'removeDiv') {
+        console.log(event.target.className);
+        console.log('hmm');
+        return;
+    }
+
+    console.log(event.target.className);
+});
+// console.log(libraryContainer.nodeName.attr('removeDiv'));
+
 const addBookForm = document.querySelector('#addBookForm');
 const addBookBtn = document.querySelector('#add-book-btn');
 const closeBookForm = document.querySelector('#close-book-form');
@@ -38,8 +50,12 @@ function showLibrary() {
     for (i = bookCount; i < myLibrary.length; i++) {
         console.log(myLibrary[i].info());
         let bookContainerDiv = document.createElement('div');
-        bookContainerDiv.className = 'bookContainer';
+        bookContainerDiv.className = `bookContainer`;
+        let titleId = myLibrary[i].title;
+        bookContainerDiv.id = titleId.replace(/\s+/g, '-');
+
         libraryContainer.appendChild(bookContainerDiv);
+
         let titleDiv = document.createElement('div');
         let title = document.createTextNode(myLibrary[i].title);
         titleDiv.appendChild(title);
@@ -48,12 +64,11 @@ function showLibrary() {
 
         let coverDiv = document.createElement('div');
         let coverImg = document.createElement('img');
-
         let coverImgFile = `/images/${myLibrary[i].title}.jpg`;
         doesFileExist(coverImgFile)
             ? (coverImg.src = coverImgFile)
             : (coverImg.src = `/images/blank.jpg`);
-        // let cover = document.createTextNode(myLibrary[i].cover);
+        coverImg.className = 'coverImg';
         coverDiv.appendChild(coverImg);
         coverDiv.className = 'coverDiv';
         bookContainerDiv.appendChild(coverDiv);
@@ -76,8 +91,24 @@ function showLibrary() {
         readDiv.className = 'readDiv';
         bookContainerDiv.appendChild(readDiv);
 
+        let removeDiv = document.createElement('div');
+        let removeImg = document.createElement('img');
+        removeImg.src = '/images/Remove.png';
+        removeImg.className = 'removeImg';
+        removeDiv.appendChild(removeImg);
+        removeDiv.className = 'removeDiv';
+        bookContainerDiv.appendChild(removeDiv);
+
         bookCount++;
+        // let removeBookBtn = document.querySelector('.removeDiv');
+        // removeBookBtn.addEventListener('click', () => removeBook(titleId));
     }
+    // document.querySelectorAll('.removeDiv').forEach((e) =>
+    //     e.addEventListener('click', () =>
+    //         //console.log(e.parentNode.id)
+    //         removeBook(e.parentNode.id)
+    //     )
+    // );
 }
 
 //check if file exists
@@ -97,17 +128,9 @@ Book.prototype.info = function () {
 //temp data
 const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', false);
 const book2 = new Book('A Game of Thrones', 'George R. R. Martin', '912', true);
-
 myLibrary.push(book1, book2);
 
 function saveBookToLibrary(title, author, pages, read) {
-    // let title = form.querySelector('input[name="title"]').value;
-    // let author = form.querySelector('input[name="author"]').value;
-    // let pages = form.querySelector('input[name="pages"]').value;
-    // let read = form.querySelector('input[name="read"]').checked;
-
-    // checkValidInput(title, author, pages, read) ?
-
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 
@@ -119,6 +142,21 @@ function saveBookToLibrary(title, author, pages, read) {
     addBookForm.style.display = 'none';
 
     showLibrary();
+}
+
+//remove book when x button is clicked
+function removeBook(bookTitle) {
+    document.querySelectorAll(`#${bookTitle}`).forEach((e) => e.remove());
+    bookTitleFind = bookTitle.replace(/[^a-zA-Z0-9 ]/g, ' ');
+    //console.log(myLibrary.findIndex((x) => x.title === bookTitleFind));
+    //myLibrary.findIndex((x) => x.title === bookTitleFind)
+    // console.log(findIndex((x) => x.title === bookTitleFind));
+    // let index = findIndex((x) => x.title === bookTitleFind);
+    // myLibrary.splice(index);
+    console.log(
+        myLibrary.splice(myLibrary.findIndex((x) => x.title === bookTitleFind))
+    );
+    bookCount--;
 }
 
 function checkValidInput() {
